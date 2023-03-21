e58e4200f9596d2b0fddb94b41798a7448392628
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-03-21 20:38:45 -0300

    Atualizar

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -3,9 +3,14 @@ import Counter from './assets/components/Counter';
 
 import NewYear from './assets/newyear.jpg';
 
+import useCountdown from './hooks/useCountdown';
+
 import './App.css';
 
+
 function App() {
+
+  const x = useCountdown("Jan 1, 2023 00:00:00");
   
   return (
     <div className="App" style={{ backgroundImage: `url(${NewYear})`}}>
