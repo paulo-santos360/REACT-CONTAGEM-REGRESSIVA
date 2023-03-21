d5ef3d33a8d9d0eceac7dda9020d0be7d534a498
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-03-21 20:09:00 -0300

    Atualizar IMG

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,15 +1,22 @@
 import Title from './assets/components/Title';
+import Counter from './assets/components/Counter';
+
+import NewYear from './assets/newyear.jpg';
 
 import './App.css';
 
 function App() {
   
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${NewYear})`}}>
       <div className="container">
       <Title title="Contagem regressiva para 2024" />
       <div className="countdown-container">
-        <p>2 dias</p>
+        <Counter title="Dias" number={2}/>
+        <Counter title="Horas" number={2}/>
+        <Counter title="Minutos" number={2}/>
+        <Counter title="Segundos" number={2}/>
+
       </div>
       </div>
     </div>
